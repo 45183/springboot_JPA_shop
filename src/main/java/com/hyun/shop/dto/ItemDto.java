16db1005ea3990f759096bb4b446d4aef4a0c828
45183 <hsb9743@naver.com>
2023-01-03 16:41:01 +0900

    1/3

---
+++ src/main/java/com/hyun/shop/dto/ItemDto.java
@@ -0,0 +1,25 @@
+package com.hyun.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private Long id;
+
+    private String itemName;
+
+    private Integer price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
